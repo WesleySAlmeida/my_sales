73173290e6bf7a31f79512ceda3dc4049e26942b
wesley <sw.almeida09@gmail.com>
2026-08-06 20:33:12 -0300

    feat: tolken repositorie and service

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,20 @@
+import AppError from "@shared/errors/AppError";
+import { usersRepositories } from "../database/repositories/UsersRepositories";
+import { UserTokensRepositories } from "../database/repositories/UserTokensRepositories";
+
+interface IForgotPassword {
+  email: string;
+}
+export default class SendForgotPasswordEmailService {
+  async execute({email}: IForgotPassword): Promise<void> {
+    const user = await usersRepositories.findByEmail(email);
+
+    if(!user) {
+      throw new AppError('User not found', 404);
+    }
+
+    const token = await UserTokensRepositories.generate(user.id)
+
+    console.log(token)
+  }
+}
