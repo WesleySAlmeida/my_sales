416aa987f985992745e8294b9c937f7e1439b2e5
wesley <sw.almeida09@gmail.com>
2026-08-13 22:32:28 -0300

    feat: entity order

---
+++ src/modules/orders/database/entities/Order.ts
@@ -0,0 +1,19 @@
+import { Customer } from "@modules/customers/database/entities/Customer";
+import { CreateDateColumn, Entity, JoinColumn, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('orders')
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Customer)
+    @JoinColumn({name: 'customer_id'})
+  customer: Customer
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
