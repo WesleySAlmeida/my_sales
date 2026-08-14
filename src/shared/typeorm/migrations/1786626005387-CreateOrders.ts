7d3ac0f82b70343fda06e503a8c6d88059141775
wesley <sw.almeida09@gmail.com>
2026-08-13 21:28:46 -0300

    feat: migrations orders

--- src/shared/typeorm/migrations/1786626005387-CreateOrders.ts
+++ src/shared/typeorm/migrations/1786626005387-CreateOrders.ts
@@ -1,11 +1,36 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateOrders1786626005387 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name: 'orders',
+          columns: [
+            {
+              name: 'id',
+              type: 'integer',
+              isPrimary: true,
+              isGenerated: true,
+              generationStrategy: 'increment',
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()',
+            }
+          ]
+        })
+      )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('orders');
     }
 
 }
