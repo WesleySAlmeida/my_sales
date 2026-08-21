1087d90f936986fc5c48c99754f4d6bc44bbeaa1
wesley <sw.almeida09@gmail.com>
2026-08-20 22:24:22 -0300

    feat:  class transformer

--- src/modules/users/controllers/UsersControllers.ts
+++ src/modules/users/controllers/UsersControllers.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
 import CreateUserService from "../services/CreateUserService";
 import ListUsersService from "../services/ListUsersService";
+import { instanceToInstance } from "class-transformer";
 
 
 export default class UsersControllers {
    async index(request: Request, response: Response): Promise<Response> {
     const listUserSService = new ListUsersService();
     const users = await listUserSService.execute();
-    return response.json(users);
+    return response.json(instanceToInstance(users));
    }
 
 
@@ -15,7 +16,7 @@ export default class UsersControllers {
     const {name, email, password} = request.body;
     const createUserService = new CreateUserService();
     const user = await createUserService.execute({name, email, password});
-    return response.json(user);
+    return response.json(instanceToInstance(user));
   }
 
 }
